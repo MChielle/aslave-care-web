ea1b60c366f6d0d2e95d794fc7b7a14c53e13b6b
Matheus Pires Chielle <matheuschielle@gmail.com>
2025-05-14 16:04:39 -0300

    restock pdf generation.

--- src/app/pages/reports/restock-report/restock-report.component.ts
+++ src/app/pages/reports/restock-report/restock-report.component.ts
@@ -1,16 +1,20 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
-import { MatPaginator } from '@angular/material/paginator';
-import { MatSort } from '@angular/material/sort';
-import { MatTableDataSource } from '@angular/material/table';
-import { RestockReportModel } from 'app/shared/models/report/restock-report.model';
-import { StockModel } from 'app/shared/models/stock/stock.model';
-import { StockService } from 'app/shared/services/stock/stock.service';
-import { firstValueFrom } from 'rxjs';
+import { Component, OnInit, ViewChild } from "@angular/core";
+import { MatPaginator, MatPaginatorIntl } from "@angular/material/paginator";
+import { MatSort } from "@angular/material/sort";
+import { MatTableDataSource } from "@angular/material/table";
+import { RestockReportModel } from "app/shared/models/report/restock-report.model";
+import { StockModel } from "app/shared/models/stock/stock.model";
+import { StockService } from "app/shared/services/stock/stock.service";
+import { firstValueFrom } from "rxjs";
+import { formatDate } from "@angular/common";
+import pdfMake from "pdfmake/build/pdfmake";
+import pdfFonts from "pdfmake/build/vfs_fonts";
+pdfMake.vfs = pdfFonts.pdfMake.vfs;
 
 @Component({
-  selector: 'app-restock-report',
-  templateUrl: './restock-report.component.html',
-  styleUrls: ['./restock-report.component.scss']
+  selector: "app-restock-report",
+  templateUrl: "./restock-report.component.html",
+  styleUrls: ["./restock-report.component.scss"],
 })
 export class RestockReportComponent implements OnInit {
   public propertyLenght;
@@ -21,24 +25,21 @@ export class RestockReportComponent implements OnInit {
     "supplierName",
     "stockTypeId",
     "quantity",
-    "lowerPrice", 
+    "lowerPrice",
   ];
 
   @ViewChild(MatPaginator) paginator!: MatPaginator;
   @ViewChild(MatSort) sort!: MatSort;
 
-  constructor(
-    private service: StockService<StockModel>,
-  ) { }
+  constructor(private service: StockService<StockModel>) {}
 
   ngOnInit(): void {
     this.getRestockReport();
   }
 
-  getRestockReport(){
+  getRestockReport() {
     firstValueFrom(this.service.getRestockReport())
       .then((response) => {
-        console.log(response);
         if (response.isSuccess) {
           this.stocks = response.data;
           this.reloadDataSource();
@@ -49,25 +50,162 @@ export class RestockReportComponent implements OnInit {
       });
   }
 
-  applyFilter(event: Event){
+  applyFilter(event: Event) {
     const filterValue = (event.target as HTMLInputElement).value;
     this.dataSource.filter = filterValue.trim().toLowerCase();
-    if(this.dataSource.paginator){
+    if (this.dataSource.paginator) {
       this.dataSource.paginator.firstPage();
     }
   }
 
-  downloadRestockReport(){
+  generatePdfRestockReport() {
+    const tableHeader = [
+      [
+        { text: "Nome", bold: true },
+        { text: "Fornecedor", bold: true },
+        { text: "Tipo", bold: true },
+        { text: "Quantidade", bold: true },
+        { text: "Menor Preço", bold: true },
+      ],
+      ...this.dataSource.data.map((row) => [
+        row.name?.toString() ?? "",
+        row.supplierName?.toString() ?? "",
+        row.stockTypeId?.toString() ?? "",
+        row.quantity?.toString() ?? "",
+        row.lowerPrice
+          ? `R$ ${row.lowerPrice.toLocaleString("pt-BR", {
+              minimumFractionDigits: 2,
+              maximumFractionDigits: 2,
+            })}`
+          : "R$ 0,00",
+      ]),
+    ];
+
+    let docDefinition = {
+      language: "pt-BR",
+      info: {
+        title: "Lista de compras",
+        author: "Aslave Care",
+        subject: "restock report",
+      },
+      content: [
+        {
+          text: "Lista de compras",
+          bold: true,
+          fontSize: 20,
+          alignment: "center",
+          margin: [0, 0, 0, 20],
+        },
+        {
+          text: "Relatório de suprimentos com estoque abaixo do limite.",
+        },
+        {
+          text: `Data: ${formatDate(Date.now(), "dd/MM/yyyy", "en-US")}`,
+          alignment: "right",
+          margin: [0, 10, 0, 10],
+        },
+        {
+          layout: "lightHorizontalLines",
+          style: "table",
+          margin: [0, 0, 0, 100],
+          table: {
+            headerRows: 1,
+            widths: ["auto", "auto", "auto", "*", "*"],
+            body: tableHeader,
+          },
+        },
+      ],
+    };
+
+    let pdf = pdfMake.createPdf(docDefinition);
+    console.log(pdf);
+    pdf.open();
   }
 
+  // downloadRestockReport() {
+  //   const tableHeader = [
+  //     { text: "Nome", bold: true },
+  //     { text: "Fornecedor", bold: true },
+  //     { text: "Tipo", bold: true },
+  //     { text: "Quantidade", bold: true },
+  //     { text: "Menor Preço", bold: true },
+  //   ];
+
+  //   const tableData = [
+  //     ...this.dataSource.data.map((row) => [
+  //       row.name ?? "",
+  //       row.supplierName ?? "",
+  //       row.stockTypeId ?? "",
+  //       row.quantity ?? "",
+  //       row.lowerPrice ?? "",
+  //     ]),
+  //   ];
+
+  //   const tableBody = [
+  //     {
+  //       headerRows: 1,
+  //       widths: ["*", "*", "*", "*", "auto"],
+  //       body: [
+  //         tableHeader,
+  //         //tableData
+  //       ],
+  //     },
+  //   ];
+
+  //   let docDefinition = {
+  //     language: "pt-BR",
+  //     info: {
+  //       title: "Lista de compras",
+  //       author: "Aslave Care",
+  //       subject: "restock report",
+  //     },
+  //     content: [
+  //       {
+  //         text: "Lista de compras",
+  //         bold: true,
+  //         fontSize: 20,
+  //         alignment: "center",
+  //         margin: [0, 0, 0, 20],
+  //       },
+  //       {
+  //         text: "Relatório de suprimentos com estoque abaixo do limite.",
+  //       },
+  //       {
+  //         text: `Data: ${formatDate(Date.now(), "dd/MM/yyyy", "en-US")}`,
+  //         alignment: "right",
+  //         margin: [0, 10, 0, 10],
+  //       },
+  //       {
+  //         layout: "lightHorizontalLines",
+  //         style: "table",
+  //         margin: [0, 0, 0, 100],
+  //         table: [
+  //           { text: "Nome", bold: true },
+  //           { text: "Fornecedor", bold: true },
+  //           { text: "Tipo", bold: true },
+  //           { text: "Quantidade", bold: true },
+  //           { text: "Menor Preço", bold: true },
+  //         ],
+  //       },
+  //     ],
+  //   };
+
+  //   let pdf = pdfMake.createPdf(docDefinition);
+  //   console.log(pdf);
+  //   pdf.open();
+  // }
+
   reloadDataSource() {
-    let source = this.stocks.map(x => new RestockReportModel(
-      x.name,
-      x.registerInStocks[0].registerIn.supplier.name,
-      x.stockTypeId,
-      x.quantity,
-      x.registerInStocks[0].price
-    ));
+    let source = this.stocks.map(
+      (x) =>
+        new RestockReportModel(
+          x.name,
+          x.registerInStocks[0].registerIn.supplier.name,
+          x.stockTypeId,
+          x.quantity,
+          x.registerInStocks[0].price
+        )
+    );
 
     this.dataSource = new MatTableDataSource<RestockReportModel>(source);
     this.dataSource.paginator = this.paginator;
