c43ab87597de3a79e27eb82a784c3a3a2f539fa3
Matheus Pires Chielle <matheuschielle@gmail.com>
2025-06-20 17:24:38 -0300

    Software name.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   PRODUCTION: true,
   BASE_URL: "https://aslave-care-api.onrender.com/api",
-  APPLICATION_NAME: "Aslave Care Software",
+  APPLICATION_NAME: "Lar - ILPI Software",
 };
